e5c1c28502b6dd9b903345d60f9ec3e335c17263
Arafat Husayn <hello@arafat.dev>
2020-05-18 00:24:01 +0600

    add serverless function

---
+++ api/get-summary.ts
@@ -0,0 +1,22 @@
+import { NowRequest, NowResponse } from "@now/node"
+
+import Summary from "../types/Summary"
+import prepareMessageSummary from "../utils/prepare-message-summary"
+import callSourceToGetSummary from "../utils/call-source-to-get-summary"
+import sendSummaryMessage from "../utils/send-summary-message"
+
+export default async (req: NowRequest, res: NowResponse): Promise<void> => {
+  const data: Summary = await callSourceToGetSummary()
+
+  const message: string = prepareMessageSummary(data)
+
+  const chatId: number =
+    req.body &&
+    req.body.message &&
+    req.body.message.chat &&
+    req.body.message.chat.id
+
+  await sendSummaryMessage(chatId, message)
+
+  res.send("")
+}
